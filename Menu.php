3fed388a01efaa9c836d3602aae17eee7be9aac8
Aksel5741 <aksel5741@gmail.com>
2019-12-10 19:43:28 +0300

    first commit

---
+++ Menu.php
@@ -0,0 +1,12 @@
+<?php
+echo <<<_END
+<link rel="stylesheet" type="text/css" href="menu_style.css">
+<ul>
+<li><a href="">Добавить пользователя</a></li>
+<li><a href="">Добавить группу </a></li>
+<li><a href="">Список пользователей</a></li>
+<li><a href="">Выйти</a></li>
+</ul>
+<hr>
+_END;
+
---
+++ login.php
@@ -0,0 +1,33 @@
+<?php
+define("PASSWORD",'password');
+define('LOGIN','admin');
+$data=$_POST;
+if(isset($data['try_signin'])){
+    if(isset($data['login']) && isset($data['password'])){
+        $errors=array();
+        if(LOGIN!=trim($data['login']))$errors[]='Неверний логин';
+        if(PASSWORD!=$data['password'])$errors[]='Неверний пароль';
+        if(empty($errors)){
+            header('Location:Main.php');
+        }
+        else{
+            echo '<div style="color:red;">'.array_shift($errors).'</div><hr>';
+        }
+    }
+}
+?>
+
+<form action="login.php" method="post">
+    <p>
+        <strong>Введите логин</strong>
+        <input type="text" name="login">
+    </p>
+    <p>
+        <strong>Введите пароль</strong>
+        <input type="password" name="password">
+    </p>
+    <div>
+        <button type="submit" name="try_signin">Войти</button>
+    </div>
+
+</form>
